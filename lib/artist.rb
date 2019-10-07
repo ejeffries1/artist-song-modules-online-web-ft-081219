6a3d2912a0304490ab54fdb89591041e42bda4e1
Evan Jeffries <ejeffries14@gmail.com>
2019-10-07 07:45:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,11 +9,11 @@ class Artist
   
   @@artists = []
 
-  #def initialize
+  def initialize
     #self.class.all << self
     super
     @songs = []
-  #end
+  end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
